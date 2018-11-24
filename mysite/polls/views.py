d21be4b06677f193b7d907af8e5888894f88f861
x9sheikh <x9sheikh.usama@gmail.com>
2018-11-24 08:29:33 +0500

    addChanging

---
+++ mysite/polls/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from.models import Question
+
+def index(request):
+    latestQuestion = Question.objects.order_by('-pub_date')[:5]
+    context = {'latestQuestion':latestQuestion}
+    return render(request,'/polls/index.html',context)
+
+def detail(request, questionId):
+    return HttpResponse("it is the %s Question detail" %questionId)
+
+def result(request, questionId):
+    return HttpResponse("it is the %s Question results" %questionId)
+
+def vote(request, questionId):
+    return HttpResponse("it is the %s Question vote" %questionId)
