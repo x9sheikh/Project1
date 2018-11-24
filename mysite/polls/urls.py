be27a5e0550ebacb3d992a1d15adda4c41f824ce
x9sheikh <x9sheikh.usama@gmail.com>
2018-11-24 08:21:24 +0500

    addChanging

--- mysite/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from .import views
-
-urlpatterns=[
-    url(r'^$',views.index,name='index'),
-    url(r'^(?P<questionId>[0-9]+)/$',views.detail,name='detail'),
-    url(r'^(?P<questionId>[0-9]+)/result$',views.result,name='result'),
-    url(r'^(?P<questionId>[0-9]+)/vote',views.vote,name='vote'),
-]
